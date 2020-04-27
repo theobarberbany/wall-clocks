b77f6144c81a833d46e156f3759fea2c81fcac40
Theo <theo.barberbany@pusher.com>
2020-04-27 18:18:16 +0100

    Add Timezones type
    * This adds the timezones type, along with statuses and conditions.
    * Uncommenting L83 of timezones_types.go causes parser errors when
    running 'make', ignoring

--- api/v1/timezones_types.go
+++ api/v1/timezones_types.go
@@ -1,5 +1,7 @@
 /*
 
+Copyright 2020 Theo Barber-Bany
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -16,27 +18,99 @@ limitations under the License.
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // TimezonesSpec defines the desired state of Timezones
 type TimezonesSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Clocks define the WallClocks to be created from this set of 'Timezones'.
+	// It should be populated with short codes (GMT) or locations as defined by
+	// the go Time package ("Europe/London")
+	Clocks []string `json:"clocks,omitempty"`
 }
+
+// TimezonesPhase determines the phase in which the Timezones currently is
+type TimezonesPhase string
+
+// The following TimezonesPhases enumerate all possible TimezonesPhases
+const (
+	TimezonesPhaseNew        TimezonesPhase = "New"
+	TimezonesPhaseInProgress TimezonesPhase = "InProgress"
+	TimezonesPhaseCompleted  TimezonesPhase = "Completed"
+)
 
 // TimezonesStatus defines the observed state of Timezones
 type TimezonesStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Phase is used to determine which phase of the creation cycle a Timezones
+	// is currently in.
+	Phase TimezonesPhase `json:"phase"`
+
+	// WallClocksCreated lists the names of all WallClocks created by the
+	// controller for this Timezones.
+	WallClocksCreated []string `json:"wallClocksCreated,omitempty"`
+
+	// WallClocksCreatedCount is the count of WallClocksCreated.
+	// This is used for printing in kubectl.
+	WallClocksCreatedCount int `json:"wallClocksCreatedCount,omitempty"`
+
+	// CompletionTimestamp is a timestamp for when the creation of WallClocks
+	// has completed
+	CompletionTimestamp *metav1.Time `json:"completionTimestamp,omitempty"`
+
+	// Conditions gives detailed condition information about the Timezones
+	//Conditions []TimezonesCondition `json:"conditions,omitempty"`
+}
+
+// TimezonesConditionType is the type of a TimezonesCondition
+type TimezonesConditionType string
+
+const (
+	// WallClocksCreatedType refers to whether the controller successfully
+	// created all of the required Timezoness
+	WallClocksCreatedType TimezonesConditionType = "TimezonesCreated"
+
+	// WallClocksInProgressType refers to whether the controller is currently
+	// processing WallClocks
+	WallClocksInProgressType TimezonesConditionType = "TimezonesInProgress"
+)
+
+// TimezonesConditionReason represents a valid condition reason for a Timezones
+type TimezonesConditionReason string
+
+// TimezonesCondition is a status condition for a Timezones
+type TimezonesCondition struct {
+	// Type of this condition
+	Type TimezonesConditionType `json:"type"`
+
+	// Status of this condition
+	Status corev1.ConditionStatus `json:"status"`
+
+	// this makes code gen blow up, I can't be bothered to hunt down why given
+	// time constraints
+
+	// LastUpdateTime of this condition
+	LastUpdateTime metav1.Time `json:"lastUpdateTime,omitempty"`
+
+	// LastTransitionTime of this condition
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+
+	// Reason for the current status of this condition
+	Reason TimezonesConditionReason `json:"reason,omitempty"`
+
+	// Message associated with this condition
+	Message string `json:"message,omitempty"`
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:path=timezones,scope=Cluster
 
 // Timezones is the Schema for the timezones API
+// +kubebuilder:resource:path=timezones,shortName=tzone;tz;tzs
+// +kubebuilder:printcolumn:name="WallClocks created",type="integer",JSONPath=".status.wallClocksCreatedCount",description="Number of WallClocks created"
+// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
+// +kubebuilder:printcolumn:name="Completed",type="date",JSONPath=".status.completionTimestamp",description="The time since the creation of WallClocks completed"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type Timezones struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -46,6 +120,7 @@ type Timezones struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:path=timezonesList,scope=Cluster
 
 // TimezonesList contains a list of Timezones
 type TimezonesList struct {
--- go.mod
+++ go.mod
@@ -1,12 +1,16 @@
 module github.com/ziglu/wallclocks
 
-go 1.12
+go 1.14
 
 require (
 	github.com/go-logr/logr v0.1.0
-	github.com/onsi/ginkgo v1.6.0
-	github.com/onsi/gomega v1.4.2
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	github.com/pusher/navarchos v0.1.0
+	golang.org/x/net v0.0.0-20190501004415-9ce7a6920f09
+	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	sigs.k8s.io/controller-runtime v0.2.0-beta.4
+	sigs.k8s.io/controller-tools v0.2.0-beta.4 // indirect
 )
